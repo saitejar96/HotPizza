dd6198c365fd700c939af31b45be93dabb9433c1
saitejar96 <saitejar96@gmail.com>
2017-03-09 19:15:34 +0530

    default page added

--- app/src/main/java/com/example/saiteja/hotpizza/MainActivity.java
+++ app/src/main/java/com/example/saiteja/hotpizza/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.saiteja.hotpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void getMovDesc(View v) {
+        Intent intent = new Intent(this, MoviesActivity.class);
+        EditText movie_name = (EditText)v.findViewById(R.id.editText);
+        intent.putExtra("name",movie_name.getText().toString());
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/saiteja/hotpizza/MoviesActivity.java
@@ -0,0 +1,8 @@
+package com.example.saiteja.hotpizza;
+
+/**
+ * Created by sai teja on 09-03-2017.
+ */
+
+public class MoviesActivity {
+}
