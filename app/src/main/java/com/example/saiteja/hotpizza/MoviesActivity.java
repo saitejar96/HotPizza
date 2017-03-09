a770fcbbf06814ae6c12b8cb09709ecb45774af9
saitejar96 <saitejar96@gmail.com>
2017-03-09 19:56:43 +0530

    Wrote second view

--- app/src/main/java/com/example/saiteja/hotpizza/MoviesActivity.java
+++ app/src/main/java/com/example/saiteja/hotpizza/MoviesActivity.java
@@ -1,8 +1,81 @@
 package com.example.saiteja.hotpizza;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import cz.msebera.android.httpclient.Header;
+
 /**
- * Created by sai teja on 09-03-2017.
+ * Created by sai teja on 16-12-2016.
  */
 
-public class MoviesActivity {
+public class MoviesActivity extends Activity {
+
+    private static String movie_url = "";
+    private static String movie_name = "";
+
+    public void getVideoPage(View v){
+        Intent myIntent = new Intent(this,VideoViewActivity.class);
+        myIntent.putExtra("vid_url",movie_url);
+        myIntent.putExtra("name",movie_name);
+        startActivity(myIntent);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_list);
+        Intent intent = getIntent();
+        movie_name = intent.getStringExtra("Name");
+        String movie_nameX = movie_name.replace(' ', '+');
+        ProgressBar spinner = (ProgressBar)findViewById(R.id.progressBarA);
+        spinner.setVisibility(View.VISIBLE);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://yesmovies.to/search/"+movie_nameX+".html", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+                //System.out.println("Started");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                //System.out.println("Failed");
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+                //System.out.println("Retrying  "+retryNo);
+            }
+        });
+    }
 }
